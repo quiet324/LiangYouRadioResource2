8e48e0b3063a5dec08f61d9202843d897e0bac3c
PaulZeng <510830568@qq.com>
2017-05-16 03:55:20 +0800

    Auto-commit

--- node/everyday/all.js
+++ node/everyday/all.js
@@ -47,7 +47,7 @@ results.forEach(function(artist) {
                         audio.size = artist.size;
                         audio.artistId = artist.id;
                         audio.artistName = artist.name;
-                        audio.path = "https://rawcdn.githack.com/quiet324/LiangYouRadioResource/" + commitTag + "/" + artist.shortName + "/" + fileName;
+                        audio.path = "https://rawcdn.githack.com/quiet324/LiangYouRadioResource2/" + commitTag + "/" + artist.shortName + "/" + fileName;
                         audio.id = artist.id * 1000000 + parseInt(audio.time.substring(2), 10);
 
                         fs.writeFileSync("./" + artist.shortName + audio.time + ".json", JSON.stringify(audio, null, '\t'));
